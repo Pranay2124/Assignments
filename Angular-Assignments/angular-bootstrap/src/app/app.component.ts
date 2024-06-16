ff65cccd7e4025e24e5df6dfadd63215e9dc3d36
Pranay0909 <pranaykadam996@gmail.com>
2024-06-16 11:40:09 +0530

    Shop-Cart Updates

--- Angular-Assignments/angular-bootstrap/src/app/app.component.ts
+++ Angular-Assignments/angular-bootstrap/src/app/app.component.ts
@@ -4,12 +4,13 @@ import { TableDataComponent } from '../table-data/table-data.component';
 import { ListDataComponent } from '../list-data/list-data.component';
 import { UsePipePipe } from '../use-pipe.pipe';
 import { ProfileBuilderComponent } from '../profile-builder/profile-builder.component';
+import { CartManagementComponent } from '../cart-management/cart-management.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TableDataComponent,ListDataComponent,UsePipePipe,ProfileBuilderComponent],
-  template: '<app-profile-builder></app-profile-builder>',
+  imports: [TableDataComponent,ListDataComponent,UsePipePipe,ProfileBuilderComponent,CartManagementComponent],
+  template: '<app-cart-management></app-cart-management>',
 })
 export class AppComponent {
   title = 'angular-bootstrap';
---
+++ Angular-Assignments/angular-bootstrap/src/pipes/use-pipe2.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { UsePipe2Pipe } from './use-pipe2.pipe';
+
+describe('UsePipe2Pipe', () => {
+  it('create an instance', () => {
+    const pipe = new UsePipe2Pipe();
+    expect(pipe).toBeTruthy();
+  });
+});
