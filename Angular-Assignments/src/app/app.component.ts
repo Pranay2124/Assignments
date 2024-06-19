907b27e600ba37493200e4ed2dee5eda2a78c733
Pranay0909 <pranaykadam996@gmail.com>
2024-06-19 06:00:00 +0530

    Angular-Routing

--- Angular-Assignments/src/app/app.component.ts
+++ Angular-Assignments/src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { UsePipePipe } from '../use-pipe.pipe';
-import { TaskListComponent } from '../ToDoList_Assignment/task-list/task-list.component';
-import { TableDataComponent } from '../Table_Assignment/table-data.component';
-import { ListDataComponent } from '../List_Assignment/list-data.component';
-import { ProfileBuilderComponent } from '../Profile-Builder_Assignment/profile-builder.component';
-import { CartManagementComponent } from '../Cart-Management_Assignment/cart-management.component';
-import { ShowCartComponent } from '../Cart-Management_Assignment/show-cart/show-cart.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TableDataComponent,ListDataComponent,UsePipePipe,ProfileBuilderComponent,CartManagementComponent,ShowCartComponent,TaskListComponent],
-  template: '<app-cart-management></app-cart-management>',
+  imports: [FormsModule,RouterOutlet,RouterLink],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'angular-bootstrap';
--- Angular-Assignments/src/app/app.routes.ts
+++ Angular-Assignments/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { ListDataComponent } from '../List_Assignment/list-data.component';
+import { TableDataComponent } from '../Table_Assignment/table-data.component';
+import { ProfileBuilderComponent } from '../Profile-Builder_Assignment/profile-builder.component';
+import { TaskListComponent } from '../ToDoList_Assignment/task-list/task-list.component';
+import { CartManagementComponent } from '../Cart-Management_Assignment/cart-management.component';
+import { ShowCartComponent } from '../Cart-Management_Assignment/show-cart/show-cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',component:ListDataComponent
+    },
+    {
+        path:'table',component:TableDataComponent
+    },
+    {
+        path:'profile',component:ProfileBuilderComponent
+    },
+    {
+        path:'todo',component:TaskListComponent
+    },
+    {
+        path:'cart-mang',component:CartManagementComponent,
+        children:[
+            {
+                path:'cart',component:ShowCartComponent
+            }
+        ]
+    },
+];
